70d3b0b8e04ca4e0012326f87fa49b40678c9645
Verse <verseow@gmail.com>
2023-11-05 12:16:23 -0500

    Enhanced test

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -3,10 +3,11 @@ from src.weather import Weather
 import pandas as pd
 
 class Tests:
-    
-    def test_get_temperature():
-        print("Testing get_temperature")
+
+    def test_get_temperature(self):
+        print("\nTesting get_temperature with London, UK")
         weather = Weather()
         temperature = weather.get_temperature("London, UK")
+        print("Temperature: ", temperature)
         assert temperature['dataframe'].shape[0] > 0
         assert temperature['dataframe'].shape[1] == 2
