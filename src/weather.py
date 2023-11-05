01022ead5b20b6c82c4c0f194de4f657782ca6ec
geoffreybudiman91 <geoffrey.budiman@gmail.com>
2023-11-05 08:08:47 -0500

    Modified weather.py

--- src/weather.py
+++ src/weather.py
@@ -5,51 +5,55 @@ from retry_requests import retry
 from optparse import OptionParser
 from geopy.geocoders import Nominatim
 
+class Weather:
+    def __init__(self):
+        self.client = openmeteo_requests.Client(session=retry(requests_cache.CachedSession('.cache', expire_after = 3600), retries = 5, backoff_factor = 0.2))
+        self.geolocator = Nominatim(user_agent="weather_package")
+        self.url = "https://api.open-meteo.com/v1/forecast"
 
-# Setup the Open-Meteo API client with cache and retry on error
-cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
-retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
-openmeteo = openmeteo_requests.Client(session = retry_session)
+    def get_weather_data(self, location_string):
+        # Geocode the location
+        location = self.geolocator.geocode(location_string)
+        if location is None:
+            raise ValueError("Could not geocode the location.")
 
-geolocator = Nominatim(user_agent="weather_package")
+        # Prepare request parameters
+        params = {
+            "latitude": location.latitude,
+            "longitude": location.longitude,
+            "hourly": "temperature_2m"
+        }
 
-# Make sure all required weather variables are listed here
-# The order of variables in hourly or daily is important to assign them correctly below
-url = "https://api.open-meteo.com/v1/forecast"
+        # Make the API call
+        responses = self.client.weather_api(self.url, params=params)
 
-parser = OptionParser()
-parser.add_option("-l", dest="location")
-(options, args) = parser.parse_args()
+        # Assuming the response is a single object and not a list
+        response = responses[0]
 
-location = geolocator.geocode(options.location)
+        # Extract information from the response
+        coordinates = (response.Latitude(), response.Longitude())
+        elevation = response.Elevation()
+        timezone_info = (response.Timezone(), response.TimezoneAbbreviation(), response.UtcOffsetSeconds())
 
-params = {
-	"latitude": location.latitude,
-	"longitude": location.longitude,
-	"hourly": "temperature_2m"
-}
+        # Process hourly data
+        hourly = response.Hourly()
+        hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
 
-responses = openmeteo.weather_api(url, params=params)
+        hourly_data = {
+            "date": pd.date_range(
+                start=pd.to_datetime(hourly.Time(), unit="s"),
+                end=pd.to_datetime(hourly.TimeEnd(), unit="s"),
+                freq=pd.Timedelta(seconds=hourly.Interval()),
+                inclusive="left"
+            )
+        }
 
-# Process first location. Add a for-loop for multiple locations or weather models
-response = responses[0]
-print(f"Coordinates {response.Latitude()}°E {response.Longitude()}°N")
-print(f"Elevation {response.Elevation()} m asl")
-print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
-print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+        hourly_dataframe = pd.DataFrame(data=hourly_data)
 
-# Process hourly data. The order of variables needs to be the same as requested.
-hourly = response.Hourly()
-hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
-
-hourly_data = {"date": pd.date_range(
-	start = pd.to_datetime(hourly.Time(), unit = "s"),
-	end = pd.to_datetime(hourly.TimeEnd(), unit = "s"),
-	freq = pd.Timedelta(seconds = hourly.Interval()),
-	inclusive = "left"
-)}
-hourly_data["temperature_2m"] = hourly_temperature_2m
-
-hourly_dataframe = pd.DataFrame(data = hourly_data)
-print(hourly_dataframe)
+        return {
+            'coordinates': coordinates,
+            'elevation': elevation,
+            'timezone_info': timezone_info,
+            'dataframe': hourly_dataframe
+        }
 
