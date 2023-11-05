70fae1b0cc987eb0de7354c1f72cd1411842ac94
Verse <verseow@gmail.com>
2023-11-05 12:08:18 -0500

    fixed quick typo

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -2,9 +2,10 @@ import pytest
 from src.weather import Weather
 import pandas as pd
 
-class tests:
-
+class Tests:
+    
     def test_get_temperature():
+        print("Testing get_temperature")
         weather = Weather()
         temperature = weather.get_temperature("London, UK")
         assert temperature['dataframe'].shape[0] > 0
